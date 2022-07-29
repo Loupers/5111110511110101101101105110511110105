bf1721d189bf10b111a73274e3d41b6bf56e998d
Vojtěch Žák <vojtech.zak@email.cz>
2022-07-29 16:15:55 +0200

    updated counting script

--- counter.py
+++ counter.py
@@ -1,12 +1,5 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 import sys
 
-def code(word):
-    count = 0
-    for x in word:
-        if x != ' ':
-            count += ord(x) - 96
-    return count % 31
-
 for x in sys.argv[1:]:
-    print(code(x))
+	print(sum(ord(c) - 64 for c in x.upper().replace(' ', '')) % 31)
